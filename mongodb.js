0eacf8f15c4432e1b8007d420334ee39b5f39374
zaziba.ahmed <zaziba.ahmed@trucklagbe.com>
2022-10-03 17:28:57 +0600

    Insertion and promise handling

--- mongodb.js
+++ mongodb.js
@@ -13,10 +13,48 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
 
     console.log('Connected correctly!');
     const db = client.db(databaseName) // Creats the database and return the database reference
+
     
-    db.collection('users').insertOne({
-        name: 'Zaziba',
-        age: 26
+    // db.collection('users').insertOne({
+    //     name: 'Zaziba',
+    //     age: 26
+    // }).then((result) => {
+    //     // return resolve(correct);
+    //     console.log(result);
+    // }).catch((error) => {
+    //     console.log('Unable to connect to databse');
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Abul',
+    //         age: 21
+    //     },
+    //     {
+    //         name: 'Bokrul',
+    //         age: 27
+    //     }
+    // ]).then((result) => {
+    //     // return resolve(correct);
+    //     console.log(result);
+    // }).catch((error) => {
+    //     console.log('Unable to connect to databse');
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Wash Dishes',
+            completed: false
+        },
+        {
+            description: 'Clean Room',
+            completed: true
+        },
+    ]).then((result) => {
+        // return resolve(correct);
+        console.log(result);
+    }).catch((error) => {
+        console.log('Unable to connect to databse');
     })
 });
 

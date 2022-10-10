54a3f628aea446e1c2a9e58fbf58b0109aa3dadd
zaziba.ahmed <ahmedzaziba@gmail.com>
2022-10-11 01:57:05 +0600

    Delete endpoints

--- src/index.js
+++ src/index.js
@@ -81,19 +81,17 @@ app.patch('/users/:id', async (req, res) => {
 
 app.delete('/users/:id', async (req, res) => {
 
-    const _id = req.params.id;
-    // User.findByIdAndDelete(_id);
-    // try {
-    //     const user = await User.findById(_id);
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
 
-    //     if(!user){
-    //         return res.status(404).send();
-    //     }
+        if(!user){
+            return res.status(404).send();
+        }
 
-    //     res.send(user);
-    // } catch (error) {
-    //     res.status(500).send(); 
-    // }
+        res.send(user);
+    } catch (error) {
+        res.status(500).send(); 
+    }
 })
 
 app.post('/tasks', async (req, res) => {
@@ -155,6 +153,21 @@ app.patch('/tasks/:id', async (req, res) => {
         res.send(task);
     } catch (error) {
         res.status(400).send(error); 
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+
+        if(!task){
+            return res.status(404).send();
+        }
+
+        res.send(task);
+    } catch (error) {
+        res.status(500).send(); 
     }
 })
 

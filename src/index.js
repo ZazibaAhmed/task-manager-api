59d6b3a6a75b80dd05f45a06cda65f085a29c83a
zaziba.ahmed <ahmedzaziba@gmail.com>
2022-11-07 00:31:05 +0600

    Auto-cropping and image formatting done

--- src/index.js
+++ src/index.js
@@ -6,36 +6,6 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const multer = require('multer');
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000 // 1Mb
-    },
-    fileFilter(req, file, cb){
-
-        if(!file.originalname.match(/\.(doc|docx)$/)){
-            return cb(new Error('Please upload a word doc'))
-        }
-
-        cb(undefined, true);
-
-        // cb(new Error('Please upload a PDF')) // when error occurs
-        // cb(undefined, true) // when upload is fine; Accepts it
-        // cb(undefined, false) // silently rejects the call
-    }
-})
-
-const errorMiddleware = (req, res, next) => {
-    throw new Error('From my middleware')
-}
-
-app.post('/upload', errorMiddleware, (req, res) => {
-    res.send();
-}, (error, req, res, next) => {
-    res.status(400).send({ error : error.message });
-})
-
 app.use(express.json()); //Automatically parses incoming JSON to an object
 app.use(userRouter);
 app.use(taskRouter);
